28fbdf6408b849460d34840f53e0f04b0085103d
Nic McPhee <mcphee@morris.umn.edu>
2013-10-28 17:34:46 -0500

    Fixed up some spacing.

--- src/search/LinearSearch.java
+++ src/search/LinearSearch.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class LinearSearch<T> {
     
-    public boolean search(T target, ArrayList<T> list){
+    public boolean search(T target, ArrayList<T> list) {
         for(int i=0; i<list.size(); i++){
-            if(list.get(i).equals(target)){
+            if(list.get(i).equals(target)) {
                 return true;
             }
         }
