f00e382bca71aa49a9e777eb8af265d433802a34
Peter Dolan <dolan118@morris.umn.edu>
2015-11-09 23:11:20 -0600

    Initial Production

---
+++ ThreadedSearch/src/search/LinearSearch.java
@@ -0,0 +1,15 @@
+package search;
+
+import java.util.ArrayList;
+
+public class LinearSearch<T> {
+    
+    public boolean search(T target, ArrayList<T> list){
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).equals(target)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
