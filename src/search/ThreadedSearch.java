930dde9313116ea64cb7f337b093a8d6938c7dc0
Nic McPhee <mcphee@morris.umn.edu>
2016-11-07 22:52:14 -0600

    This makes some substantial changes to the stub for ThreadedSearch
    I added a bunch of comments to try to clarify what we’re expecting people to do, and made the use of Runnable more explicit.

--- src/search/ThreadedSearch.java
+++ src/search/ThreadedSearch.java
@@ -2,36 +2,70 @@ package search;
 
 import java.util.ArrayList;
 
-public class ThreadedSearch<T> {
-    
+public class ThreadedSearch<T> implements Runnable {
+
     private T storedTarget;
     private ArrayList<T> storedList;
     private int storedStart;
     private int storedEnd;
     private Answer storedAnswer;
-    
+
     public ThreadedSearch() {
     }
-    
+
     private ThreadedSearch(T target, ArrayList<T> list, int start, int end, Answer answer) {
         storedTarget=target;
-        storedList=list; 
+        storedList=list;
         storedStart=start;
         storedEnd=end;
         storedAnswer=answer;
     }
-    
-    // Assume list size is divisible by numThreads to start
+
+    /**
+     * Searches `list` in parallel using `numThreads` threads.
+     *
+     * You can assume that the list size is divisible by `numThreads`
+     */
     public boolean parSearch(int numThreads, T target, ArrayList<T> list) {
-        return false;
+      /*
+       * First construct an instance of the `Answer` inner class. This will
+       * be how the threads you're about to create will "communicate". They
+       * will all have access to this one shared instance of `Answer`, where
+       * they can update the `answer` field inside that instance.
+       *
+       * Then construct `numThreads` instances of this class (`ThreadedSearch`)
+       * using the 5 argument constructor for the class. You'll hand each of
+       * them the same `target`, `list`, and `answer`. What will be different
+       * about each instance is their `start` and `end` values, which you'll
+       * use to give each instance the "job" of searching a different segment
+       * of the list. If, for example, the list has length 100 and you have
+       * 4 threads, you would give the four threads the ranges [0, 25), [25, 50),
+       * [50, 75), and [75, 100) as their sections to search.
+       *
+       * You then construct `numThreads`, each of which is given a different
+       * instance of this class as its `Runnable`. Then start each of those
+       * threads, wait for them to all terminate, and then return the answer
+       * in the shared `Answer` instance.
+       */
+      return false;
     }
-    
-    private void search(T target, ArrayList<T> list, int start, int end) {
-        
+
+    public void run() {
+
     }
-    
+
     private class Answer {
-        public boolean answer=false;
+        private boolean answer = false;
+
+        public boolean getAnswer() {
+          return answer;
+        }
+
+        // This has to be synchronized to ensure that no two threads modify
+        // this at the same time, possibly causing race conditions.
+        public synchronized void setAnswer(boolean newAnswer) {
+          answer = newAnswer;
+        }
     }
 
 }
