e92313d4c559e9eb2a400e4a8e2fb4716ecfb5fc
Nic McPhee <mcphee@morris.umn.edu>
2016-11-07 23:29:03 -0600

    Clean up `stored…` names
    I cleaned up all the annoying `stored…` field names, and changed the field/argument/variable `start` to `begin` in an effort to reduce confusion with the `Thread.start()` method.
    
    I also added a `throws InterruptedException` to `parSearch`.

--- src/search/ThreadedSearch.java
+++ src/search/ThreadedSearch.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 
 public class ThreadedSearch<T> implements Runnable {
 
-  private T storedTarget;
-  private ArrayList<T> storedList;
-  private int storedStart;
-  private int storedEnd;
-  private Answer storedAnswer;
+  private T target;
+  private ArrayList<T> list;
+  private int begin;
+  private int end;
+  private Answer answer;
 
   public ThreadedSearch() {
   }
 
-  private ThreadedSearch(T target, ArrayList<T> list, int start, int end, Answer answer) {
-    storedTarget=target;
-    storedList=list;
-    storedStart=start;
-    storedEnd=end;
-    storedAnswer=answer;
+  private ThreadedSearch(T target, ArrayList<T> list, int begin, int end, Answer answer) {
+    this.target=target;
+    this.list=list;
+    this.begin=begin;
+    this.end=end;
+    this.answer=answer;
   }
 
   /**
@@ -26,7 +26,7 @@ public class ThreadedSearch<T> implements Runnable {
   *
   * You can assume that the list size is divisible by `numThreads`
   */
-  public boolean parSearch(int numThreads, T target, ArrayList<T> list) {
+  public boolean parSearch(int numThreads, T target, ArrayList<T> list) throws InterruptedException {
     /*
     * First construct an instance of the `Answer` inner class. This will
     * be how the threads you're about to create will "communicate". They
@@ -36,7 +36,7 @@ public class ThreadedSearch<T> implements Runnable {
     * Then construct `numThreads` instances of this class (`ThreadedSearch`)
     * using the 5 argument constructor for the class. You'll hand each of
     * them the same `target`, `list`, and `answer`. What will be different
-    * about each instance is their `start` and `end` values, which you'll
+    * about each instance is their `begin` and `end` values, which you'll
     * use to give each instance the "job" of searching a different segment
     * of the list. If, for example, the list has length 100 and you have
     * 4 threads, you would give the four threads the ranges [0, 25), [25, 50),
