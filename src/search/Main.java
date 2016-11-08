e99ab1e1e93fd5b56cf825465c8e067dc09fff75
Nic McPhee <mcphee@morris.umn.edu>
2016-11-07 23:21:12 -0600

    Remove the magic number
    I had 10K as a magic number and this turns that into a named constant. I also added `throws` for the exceptions that will come up when we set up the threading.

--- src/search/Main.java
+++ src/search/Main.java
@@ -4,31 +4,32 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
-    
-   public static void main(String[] args) {
+
+   public static void main(String[] args) throws InterruptedException {
+       final int ARRAY_SIZE = 10000;
        Random random = new Random();
        ArrayList<Integer> numbers = new ArrayList<Integer>();
-       for (int i=0; i<10000; ++i) {
-           numbers.add(random.nextInt(10000));
+       for (int i=0; i<ARRAY_SIZE; ++i) {
+           numbers.add(random.nextInt(ARRAY_SIZE));
        }
        System.out.println(searchArray(numbers.get(1), numbers));
        System.out.println(searchArray(numbers.get(5), numbers));
-       System.out.println(searchArray(numbers.get(900), numbers));       
+       System.out.println(searchArray(numbers.get(900), numbers));
        System.out.println(searchArray(numbers.get(3200), numbers));
        System.out.println(searchArray(numbers.get(7400), numbers));
        System.out.println(searchArray(numbers.get(9876), numbers));
        System.out.println(searchArray(2000000, numbers));
        System.out.println(searchArray(-45, numbers));
    }
-   
-   private static boolean searchArray(int target, ArrayList<Integer> list) {
+
+   private static boolean searchArray(int target, ArrayList<Integer> list) throws InterruptedException {
        // You can replace ThreadedSearch with LinearSearch to see this work with
        // the given linear search code.
        ThreadedSearch<Integer> searcher=new ThreadedSearch<Integer>();
        // This specifies 4 threads for the tests. It would be a good idea to play
        // with this and see how that changes things. Keep in mind that your number
        // of threads *may* need to evenly divide the length of the list being
-       // searched (10,000 in this case).
+       // searched (ARRAY_SIZE in this case).
        return searcher.parSearch(4, target, list);
    }
 
